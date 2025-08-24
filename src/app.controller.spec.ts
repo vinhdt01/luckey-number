20f35718f3ae47688ad255dff1d09d103e8b4bfd
Denis <thevinh.dao@stengg.com>
2025-08-24 21:48:55 +0700

    change text

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello Bình Thạnh!"', () => {
+      expect(appController.getHello()).toBe('Hello Bình Thạnh!');
     });
   });
 });
